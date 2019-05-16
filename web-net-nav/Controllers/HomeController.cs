c546cd498a342da13ff6490410c01ac303b3d454
Terry Mace <terence.h.mace@gmail.com>
2019-05-16 16:26:47 +0100

    Added functionality to input a URL and for an instance of chrome to open and load that URL

--- web-net-nav/Controllers/HomeController.cs
+++ web-net-nav/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using web_net_nav.Models;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace web_net_nav.Controllers
 {
@@ -13,6 +15,15 @@ namespace web_net_nav.Controllers
         public IActionResult Index()
         {
             return View("Index");
+        }
+
+        public IActionResult RunReceipe(string url)
+        {
+            var driver = new ChromeDriver(@"C:\Projects\web-net-nav\drivers");
+
+            driver.Navigate().GoToUrl(url);
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

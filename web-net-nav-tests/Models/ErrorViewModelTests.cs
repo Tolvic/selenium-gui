c655cb79a895e8510d310c6bf5b40fc71230db14
Terry Mace <terence.h.mace@gmail.com>
2019-05-16 13:42:58 +0100

    Added tests for ErrorViewModel

---
+++ web-net-nav-tests/Models/ErrorViewModelTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc;
+using web_net_nav.Models;
+
+
+namespace Tests
+{
+    [TestFixture]
+    public class ErrorViewModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShowRequestIdShouldReturnFalseWhenRequestIdIsSetToAnEmptyString()
+        {
+            // Arrange
+            var ErrorViewModel = new ErrorViewModel();
+            ErrorViewModel.RequestId = "";
+
+            // Act
+            var result = ErrorViewModel.ShowRequestId;
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void ShowRequestIdShouldReturnFalseWhenRequestIdIsSetToNull()
+        {
+            // Arrange
+            var ErrorViewModel = new ErrorViewModel();
+            ErrorViewModel.RequestId = null;
+
+            // Act
+            var result = ErrorViewModel.ShowRequestId;
+
+            // Assert
+            Assert.That(result, Is.False);
+        }
+    }
+}

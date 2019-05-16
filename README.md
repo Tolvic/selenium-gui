d99b825f962d5d72b074a9f135a75b227132723c
Terry Mace <terence.h.mace@gmail.com>
2019-05-16 15:18:55 +0100

    set up functional tests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# web-net-nav
+# Web App Navigator
 
+## Functional Tests
+To run functional tests you will need to run two the solution in two instances of Visual Studio. In the first instance run the application and in the second instance, run the functional tests. 
---
+++ web-net-nav.functiontests/FirstTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace web_net_nav.FunctionalTests
+{
+    [TestFixture]
+    class MetaDataTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Init()
+        {
+            _driver = new ChromeDriver(@"C:\Projects\web-net-nav\drivers");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _driver.Close();
+        }
+
+        [Test]
+        public void IndexHasTitle()
+        {
+            // Arrange
+
+            var url = "https://localhost:44321/";
+
+            // Act
+            _driver.Navigate().GoToUrl(url);
+            var result = _driver.Title;
+
+
+            // Assert
+            Assert.That(result, Does.Contain("home page").IgnoreCase);
+
+        }
+    }
+}

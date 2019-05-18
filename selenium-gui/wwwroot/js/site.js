43209eecf555e588994295c28e26e948460bccea
Terry <terence.h.mace@gmail.com>
2019-05-18 16:56:01 +0100

    created AJAX request

--- selenium-gui/wwwroot/js/site.js
+++ selenium-gui/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    RunSequence();
+})
+
+function RunSequence() {
+    $("#run-sequence").click(function () {
+        SendRunSequence();
+        console.log("run sequence")
+    })
+}
+
+function SendRunSequence() {
+    console.log("send run sequence")
+    $.ajax({
+        type: "GET",
+        url: "/Sequence/Run",
+        data: {
+            gotToUrl: "https://google.com"
+        },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            alert("Finsihed")
+        }
+    });
+}

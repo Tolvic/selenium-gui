304b5bcfd5a2227cf8cc73fe7ad5dee004916e68
Terry Mace <terence.h.mace@gmail.com>
2019-05-22 23:57:39 +0100

    Jasmine testing set up

---
+++ selenium-gui.tests/Scripts/SequenceTests.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../selenium-gui/wwwroot/lib/jquery/dist/jquery.min.js"/>
+/// <reference path="../../selenium-gui/wwwroot/js/Sequence.js"/>
+
+describe("Sequence.js", function () {
+    beforeEach(function () {
+        var mockHtml;
+    });
+
+    afterEach(function () {
+       
+    });
+
+    function setup() {
+    
+    }
+
+    it("my first test", function() {
+        // Arrange
+
+        // Acc
+
+        // Asset
+        getStepType();
+    });
+
+});

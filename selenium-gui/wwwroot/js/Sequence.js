71ce4b2b3582faa4a25b5b704175dbe65efedfb6
Terry Mace <terence.h.mace@gmail.com>
2019-05-31 15:56:27 +0100

    Moved to revealing module design pattern

--- selenium-gui/wwwroot/js/Sequence.js
+++ selenium-gui/wwwroot/js/Sequence.js
@@ -1,55 +1,68 @@
-﻿$(document).ready(function () {
-    RunSequence();
+﻿var sequence = (function () {
+    // Public functions
+    function runSequence() {
+        $("#run-sequence").click(function () {
+            var sequenceData = getSequenceData();
+            sendRunSequence(sequenceData);
+        });
+    }
+
+    // private functions
+    function getSequenceData() {
+        var sequenceData = [];
+        $(".step").each(function () {
+            var type = getStepType($(this));
+            var options = getStepOptions($(this));
+            var stepObject = stepObjectInit(type, options);
+
+            sequenceData.push(JSON.stringify(stepObject));
+        });
+
+        return sequenceData;
+    }
+
+    function getStepType(step) {
+        return step.contents().find(".step-type").val();
+    }
+
+    function getStepOptions(step) {
+        var options = [];
+
+        step.find(".step-input").each(function () {
+            options.push(($(this).val()));
+        });
+
+        return options;
+    }
+
+    function stepObjectInit(type, parameters) {
+        return { Type: type, Parameters: parameters }
+    }
+
+    function sendRunSequence(sequenceData) {
+        $.ajax({
+            type: "GET",
+            url: "/Sequence/Run",
+            data: {
+                sequenceData: JSON.stringify(sequenceData)
+            },
+            contentType: 'application/json',
+            dataType: "json",
+            success: function (response) {
+                alert("Finsihed");
+            }
+        });
+    }
+
+    // Expose functions
+    return {
+        runSequence: runSequence
+    }
+})();
+
+$(document).ready(function () {
+    sequence.runSequence();
 });
 
-function GetSequenceData() {
-    var sequenceData = [];
-    $(".step").each(function () {
-        var type = getStepType($(this));
-        var options = getStepOptions($(this));
-        var stepObject = stepObjectInit(type, options);
-        sequenceData.push(JSON.stringify(stepObject));
-    });
-    console.log(sequenceData);
-    return JSON.stringify(sequenceData);
-}
 
-function RunSequence() {
-    $("#run-sequence").click(function () {
-        var sequenceData = GetSequenceData();
-        SendRunSequence(sequenceData);
-    });
-}
 
-function getStepType(step) {
-    return step.contents().find(".step-type").val();
-}
-
-function getStepOptions(step) {
-    var options = [];
-
-    step.find(".step-input").each(function () {
-        options.push(($(this).val()));
-    });
-
-    return options;
-}
-
-function stepObjectInit(type, parameters) {
-    return { Type: type, Parameters: parameters }
-}
-
-function SendRunSequence(sequenceData) {
-    $.ajax({
-        type: "GET",
-        url: "/Sequence/Run",
-        data: {
-            sequenceData: sequenceData
-        },
-        contentType: 'application/json',
-        dataType: "json",
-        success: function (response) {
-            alert("Finsihed");
-        }
-    });
-}

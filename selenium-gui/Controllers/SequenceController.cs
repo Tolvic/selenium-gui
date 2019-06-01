e13cfa93f42d9e020821ad77697a8407aede6acc
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 19:22:41 +0100

    sequence now built using build method

--- selenium-gui/Controllers/SequenceController.cs
+++ selenium-gui/Controllers/SequenceController.cs
@@ -15,25 +15,7 @@ namespace selenium_gui.Controllers
 
         public IActionResult Run(string sequenceData)
         {
-            if (string.IsNullOrWhiteSpace(sequenceData))
-            {
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(sequenceData));
-            }
-
-            List<string> jsonSteps = JsonConvert.DeserializeObject<List<string>>(sequenceData);
-
-            List<Step> steps = new List<Step>();
-
-
-            foreach (var step in jsonSteps)
-            {
-                steps.Add(JsonConvert.DeserializeObject<Step>(step));
-            }
-
-            var sequence = new Sequence
-            {
-                Steps = steps
-            };
+            var sequence = new selenium_gui.Models.Sequence().Build(sequenceData);
 
             var driver = new ChromeDriver(@"C:\Projects\selenium-gui\drivers");
 

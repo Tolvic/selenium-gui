98ad531c445f01aa024042e43a7f79f7458e2992
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 19:38:16 +0100

    Created interface

---
+++ selenium-gui/Interfaces/ISequence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using selenium_gui.Models;
+
+namespace selenium_gui.Interfaces
+{
+    interface ISequence
+    {
+        Sequence Build(string sequenceData);
+
+        void Run();
+    }
+}

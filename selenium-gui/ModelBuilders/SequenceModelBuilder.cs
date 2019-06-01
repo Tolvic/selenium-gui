0f1211195f37cffe3fd336f52ee461bcf93ed4a3
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 20:10:45 +0100

    created interface

---
+++ selenium-gui/ModelBuilders/SequenceModelBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using selenium_gui.Interfaces;
+using selenium_gui.Models;
+
+namespace selenium_gui.ModelBuilders
+{
+    public class SequenceModelBuilder : ISequenceModelBuilder
+    {
+
+        public Sequence Build(string sequenceData)
+        {
+            if (string.IsNullOrWhiteSpace(sequenceData))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(sequenceData));
+            }
+
+            List<string> jsonSteps = JsonConvert.DeserializeObject<List<string>>(sequenceData);
+
+            List<Step> steps = new List<Step>();
+
+
+            foreach (var step in jsonSteps)
+            {
+                steps.Add(JsonConvert.DeserializeObject<Step>(step));
+            }
+
+            return new Sequence
+            {
+                Steps = steps
+            };
+        }
+    }
+}

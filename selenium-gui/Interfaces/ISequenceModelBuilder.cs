0f268f15e7d58f06f6768e5236fb325c3c7c56bc
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 20:10:55 +0100

    created model builder

---
+++ selenium-gui/Interfaces/ISequenceModelBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using selenium_gui.Models;
+
+namespace selenium_gui.Interfaces
+{
+    public interface ISequenceModelBuilder
+    {
+        Sequence Build(string sequenceData);
+    }
+}

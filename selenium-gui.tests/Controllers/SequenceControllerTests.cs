6c170a7d7c6866682414e23d7e1788d360f17536
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 21:10:27 +0100

    mocked sequence model builder

---
+++ selenium-gui.tests/Controllers/SequenceControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using selenium_gui.Interfaces;
+using selenium_gui.Models;
+
+namespace selenium_gui.tests.Controllers
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class SequenceControllerTests
+    {
+
+        private Mock<ISequenceModelBuilder> _sequenceModelBuilderMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sequenceModelBuilderMock = new Mock<ISequenceModelBuilder>();
+            _sequenceModelBuilderMock.Setup(x => x.Build(It.IsAny<string>())).Returns(new Sequence());
+        }
+
+        [Test]
+        public void SequenceTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}

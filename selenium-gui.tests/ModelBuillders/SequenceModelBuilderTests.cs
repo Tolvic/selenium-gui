22bae72461ea658273e6643f015f8b4d8999dd76
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 23:06:31 +0100

    test that build method throws argument exception when passed an empty string

---
+++ selenium-gui.tests/ModelBuillders/SequenceModelBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using NUnit.Framework;
+using selenium_gui.ModelBuilders;
+
+namespace selenium_gui.tests.ModelBuillders
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class SequenceModelBuilderTests
+    {
+        private SequenceModelBuilder _sequenceModelBuilder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sequenceModelBuilder = new SequenceModelBuilder();
+        }
+
+        [Test]
+        public void ThrowsExceptionWhenPassedEmptyString()
+        {
+            // Arrange
+            var sequenceData = string.Empty;
+            
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentException>(() => _sequenceModelBuilder.Build(sequenceData));
+        }
+
+
+    }
+}

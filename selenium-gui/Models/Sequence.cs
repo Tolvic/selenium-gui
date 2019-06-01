ec7a3433d935cca9a69b6437c011e71d953d8113
Terry Mace <terence.h.mace@gmail.com>
2019-06-01 19:16:30 +0100

    Update Sequence.cs
    created build method

--- selenium-gui/Models/Sequence.cs
+++ selenium-gui/Models/Sequence.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace selenium_gui.Models
 {
     public class Sequence
     {
         public List<Step> Steps { get; set; }
+
+        public Sequence Build(string sequenceData)
+        {
+            if (string.IsNullOrWhiteSpace(sequenceData))
+            {
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(sequenceData));
+            }
+
+            List<string> jsonSteps = JsonConvert.DeserializeObject<List<string>>(sequenceData);
+
+            List<Step> steps = new List<Step>();
+
+
+            foreach (var step in jsonSteps)
+            {
+                steps.Add(JsonConvert.DeserializeObject<Step>(step));
+            }
+
+            var sequence = new Sequence
+            {
+                Steps = steps
+            };
+        }
     }
 }

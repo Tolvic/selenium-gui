739f0da6d8cfdb398d3f7c165b5167cf213a7662
Terry Mace <terence.h.mace@gmail.com>
2019-06-03 22:56:51 +0100

    Created TestSuite and Test for Build returning a ChromeDriver

---
+++ selenium-gui.tests/ModelBuillders/DriverModelBuilderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using selenium_gui.ModelBuilders;
+
+namespace selenium_gui.tests.ModelBuillders
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class DriverModelBuilderTests
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _webDriver.Close();
+        }
+
+        [Test]
+        public void BuildReturnsChromeDirver()
+        {
+            // Arrange
+            var driverModelBuilder = new DriverModelBuilder();
+
+            // Act
+            _webDriver = driverModelBuilder.Build();
+
+            // Assert
+            Assert.IsInstanceOf<ChromeDriver>(_webDriver);
+        }
+
+    }
+}

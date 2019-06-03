6666580d6fb0433f7889859c906951926986f982
Terry Mace <terence.h.mace@gmail.com>
2019-06-03 23:05:44 +0100

    created test suite

---
+++ selenium-gui.tests/Models/SequenceTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using NUnit.Framework;
+
+namespace selenium_gui.tests.Models
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    class SequenceTests
+    {
+        [Test]
+        public void ThrowsExceptionWhenPassedEmptyString()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}

b5256928b8424df98c8f2c5ce709b2b065ee9082
Terry Mace <terence.h.mace@gmail.com>
2019-06-03 22:11:43 +0100

    Created interface with build method

---
+++ selenium-gui/Interfaces/IDriverModelBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace selenium_gui.Interfaces
+{
+    public interface IDriverModelBuilder
+    {
+        IWebDriver Build();
+    }
+}

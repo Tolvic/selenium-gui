47b4e7e009f1712f3fd70160df9d337ae1cc247a
Terry Mace <terence.h.mace@gmail.com>
2019-06-03 23:53:47 +0100

    created test for Run calling navigate for each step in sequence

--- selenium-gui.tests/Models/SequenceTests.cs
+++ selenium-gui.tests/Models/SequenceTests.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using Moq;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using selenium_gui.Models;
 
 namespace selenium_gui.tests.Models
 {
@@ -10,14 +13,51 @@ namespace selenium_gui.tests.Models
     [TestFixture]
     class SequenceTests
     {
+        private Mock<INavigation> _driverNavigationMock;
+        private Mock<IWebDriver> _driverMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driverNavigationMock = new Mock<INavigation>();
+            _driverMock = new Mock<IWebDriver>();
+            _driverMock.Setup(x => x.Navigate()).Returns(_driverNavigationMock.Object);
+        }
+
         [Test]
-        public void ThrowsExceptionWhenPassedEmptyString()
+        public void RunCallsDriverNavigateMethodOnEachStep()
         {
             // Arrange
+            var sequence = new Sequence()
+            {
+                Driver = _driverMock.Object,
+                Steps = new List<Step>() {
+                    new Step()
+                    {
+                    Type = "Go To URL",
+                    Parameters = new List<string>()
+                        {
+                            "https://google.com"
+                        }
+
+                    },
+                    new Step()
+                    {
+                        Type = "Go To URL",
+                        Parameters = new List<string>()
+                        {
+                            "https://google.com"
+                        }
+
+                    }
+                }
+            };
 
             // Act
+            sequence.Run();
 
             // Assert
+            _driverMock.Verify(x => x.Navigate(), Times.Exactly(2));
         }
     }
 }

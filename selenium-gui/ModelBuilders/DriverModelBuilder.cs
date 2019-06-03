002cc9dc5542b142c32574e186f90240cd7f9475
Terry Mace <terence.h.mace@gmail.com>
2019-06-03 22:12:24 +0100

    Created DriverModelBuilder

---
+++ selenium-gui/ModelBuilders/DriverModelBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using selenium_gui.Interfaces;
+
+namespace selenium_gui.ModelBuilders
+{
+    public class DriverModelBuilder : IDriverModelBuilder
+    {
+        public IWebDriver Build()
+        {
+            return new ChromeDriver(@"C:\Projects\selenium-gui\drivers");
+        }
+    }
+}

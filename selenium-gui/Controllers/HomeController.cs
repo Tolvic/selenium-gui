c1b99e7cd7e14b3c02fb522871ac2222ed6d9693
Terry Mace <terence.h.mace@gmail.com>
2019-06-14 14:22:31 +0100

    Created Demo Page

--- selenium-gui/Controllers/HomeController.cs
+++ selenium-gui/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace selenium_gui.Controllers
             return View("Index");
         }
 
+        public IActionResult Demo()
+        {
+            return View("Demo");
+        }
+
         [ExcludeFromCodeCoverage]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

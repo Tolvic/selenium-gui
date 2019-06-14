cde810ab0a38451e3596ac9ea5c3c081b60d1974
Terry Mace <terence.h.mace@gmail.com>
2019-06-14 16:15:38 +0100

    Added copy JSON to clipboard

---
+++ selenium-gui/wwwroot/js/Demo.js
@@ -0,0 +1,27 @@
+﻿var Demo = (function () {
+    // Public functions
+    function setUpCopyJsonEventBindings() {
+        
+    }
+
+    // private functions
+    function setUpCopyJsonEventBindings() {
+        $(".copy-json").click(function () {
+            var code = $(this).closest($("div.example")).find("code").text();
+            var clipboard = $("#clipboard");
+            clipboard.val(code);
+            clipboard.show();
+            clipboard.select();
+            document.execCommand("copy");
+            clipboard.hide();
+        });
+    }
+
+    return {
+        setUpCopyJsonEventBindings: setUpCopyJsonEventBindings
+    }
+})();
+
+$(document).ready(function () {
+    Demo.setUpCopyJsonEventBindings();
+});
